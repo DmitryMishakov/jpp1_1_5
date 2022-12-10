c71358788543114cff5b32e37440e345491780dd
Dmitry Mishakov <welzevul0@gmail.com>
2022-12-10 18:10:03 +0300

    added setter on age

--- src/main/java/com/mishakov/model/User.java
+++ src/main/java/com/mishakov/model/User.java
@@ -57,6 +57,10 @@ public class User {
         return age;
     }
 
+    public void setAge(Byte age) {
+        this.age = age;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
