ebfd591fa2fb6dcb63f7308bc6e252e2beada9ac
Dmitry Mishakov <welzevul0@gmail.com>
2022-12-10 03:27:40 +0300

    final task

--- src/main/java/com/mishakov/Main.java
+++ src/main/java/com/mishakov/Main.java
@@ -1,12 +1,18 @@
 package com.mishakov;
 
-import com.mishakov.dao.UserDao;
-import com.mishakov.dao.UserDaoJDBCImpl;
+import com.mishakov.service.UserService;
+import com.mishakov.service.UserServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
-        UserDao userDao = new UserDaoJDBCImpl();
-        userDao.createUsersTable();
-        userDao.createUsersTable();
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Ivan", "Ivanov", (byte) 12);
+        userService.saveUser("Tom", "Tomanov", (byte) 23);
+        userService.saveUser("Sam", "Samanov", (byte) 34);
+        userService.saveUser("Jack", "Jackanov", (byte) 45);
+        userService.getAllUsers().forEach(System.out :: println);
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
