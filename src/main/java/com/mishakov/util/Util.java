2f6deddd286ea8e3cd5d74822787ff960bb4e0bb
Dmitry Mishakov <welzevul0@gmail.com>
2022-12-12 03:51:46 +0300

    Create Session factory for Hibernate, I'd try to make singletone of SessionFactory

--- src/main/java/com/mishakov/util/Util.java
+++ src/main/java/com/mishakov/util/Util.java
@@ -1,11 +1,16 @@
 package com.mishakov.util;
 
+import com.mishakov.model.User;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
 
@@ -32,14 +37,37 @@ public class Util {
     }
 
 // Hibernate part
-    private static final SessionFactory sessionFactory = buildSessionFactory();
+    private static final String DIALECT = "org.hibernate.dialect.MySQLDialect";
 
-    private static SessionFactory buildSessionFactory() {
-        try {
-            return new Configuration().configure().buildSessionFactory();
-        } catch (Throwable ex) {
-            System.err.println("Initial SessionFactory creation failed " + ex);
-            throw new ExceptionInInitializerError(ex);
+    private static SessionFactory sessionFactory = buildMySessionFactory();
+
+    private static SessionFactory buildMySessionFactory() {
+        if(sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, DRIVER);
+                settings.put(Environment.URL, URL);
+                settings.put(Environment.USER, USER);
+                settings.put(Environment.PASS, PASSWORD);
+                settings.put(Environment.DIALECT, DIALECT);
+                settings.put(Environment.SHOW_SQL, true); // не зыбыть потом удалить
+                settings.put(Environment.HBM2DDL_AUTO, "update");
+
+                configuration.setProperties(settings);
+
+                configuration.addAnnotatedClass(User.class);
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+
+                return new Configuration().buildSessionFactory(serviceRegistry);
+            } catch (Throwable ex) {
+                System.err.println("Initial SessionFactory creation failed " + ex);
+                throw new ExceptionInInitializerError(ex);
+            }
+        } else {
+            return sessionFactory;
         }
     }
 
